f1f0010afe0d449dc2d550389c6f76a8afd45193
CalvinHaynes <chx1006488386@163.com>
2021-07-15 23:31:39 +0800

    添加primes.c lab

--- Lab01-Xv6 and Unix utilities/core/pingpong.c
+++ Lab01-Xv6 and Unix utilities/core/pingpong.c
@@ -7,17 +7,6 @@
 
 //pingpong.c(easy)
 
-// 要求如下：
-// Write a program that uses UNIX system calls to 
-// ''ping-pong'' a byte between two processes over a pair of pipes, 
-// one for each direction. The parent should send a byte to the child;
-//  the child should print "<pid>: received ping", 
-//  where <pid> is its process ID, write the byte on the pipe to the parent, 
-//  and exit; the parent should read the byte from the child, 
-//  print "<pid>: received pong", and exit. 
-//  Your solution should be in the file user/pingpong.c.
-
-
 #include<kernel/types.h>
 #include<user/user.h>
 
